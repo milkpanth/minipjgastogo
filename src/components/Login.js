9c2e512b8981b23f2657e7e6e1c3a82289b7d2d9
milkpanth <milkpanth@yahoo.com>
2020-04-14 16:38:35 +0700

    css

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,86 +1,13 @@
-// import React from "react";
-// import {
-//   MDBContainer,
-//   MDBRow,
-//   MDBCol,
-//   MDBCard,
-//   MDBCardBody,
-//   MDBModalFooter,
-//   MDBIcon,
-//   MDBCardHeader,
-//   MDBBtn
-// } from "mdbreact";
-// import '@fortawesome/fontawesome-free/css/all.min.css';
-// import "bootstrap-css-only/css/bootstrap.min.css";
-// import "mdbreact/dist/css/mdb.css";
-
-// const LoginPage = () => {
-// return (
-// <MDBContainer>
-//       <MDBRow>
-//         <MDBCol md="6">
-//           <MDBCard>
-//             <MDBCardBody>
-//               <MDBCardHeader className="form-header warm-flame-gradient rounded">
-//                 <h3 className="my-3">
-//                   <MDBIcon icon="lock" /> Login:
-//                 </h3>
-//               </MDBCardHeader>
-//               <label
-//                 htmlFor="defaultFormEmailEx"
-//                 className="grey-text font-weight-light"
-//               >
-//                 Your email
-//               </label>
-//               <input
-//                 type="email"
-//                 id="defaultFormEmailEx"
-//                 className="form-control"
-//               />
-
-//               <label
-//                 htmlFor="defaultFormPasswordEx"
-//                 className="grey-text font-weight-light"
-//               >
-//                 Your password
-//               </label>
-//               <input
-//                 type="password"
-//                 id="defaultFormPasswordEx"
-//                 className="form-control"
-//               />
-
-//               <div className="text-center mt-4">
-//                 <MDBBtn color="deep-orange" className="mb-3" type="submit">
-//                   Login
-//                 </MDBBtn>
-//               </div>
-
-//               <MDBModalFooter>
-//                 <div className="font-weight-light">
-//                   <p>Not a member? Sign Up</p>
-//                   <p>Forgot Password?</p>
-//                 </div>
-//               </MDBModalFooter>
-//             </MDBCardBody>
-//           </MDBCard>
-//         </MDBCol>
-//       </MDBRow>
-//     </MDBContainer>
-// );
-// };
-
-// export default LoginPage;
-
-
 import React from "react";
 import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBInput } from 'mdbreact';
+import './Login.css'
+
 
 const LoginPage = () => {
   return (
-    <MDBContainer>
+    <MDBContainer className="LoginForm">
       <MDBRow>
-        <MDBCol md='6'>
+        <MDBCol md='8'>
           <MDBCard
             className='card-image'
             style={{
